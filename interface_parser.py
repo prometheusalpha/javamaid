b45a15d76ec29f158dc683d77d665b21103702ff
prometheusalpha <hoangnguyen.hn212.hn@gmail.com>
2023-11-13 16:22:10 +0700

    feat: update logic only display import and extends/implement if selected

--- interface_parser.py
+++ interface_parser.py
@@ -1,20 +1,44 @@
 import javalang
 
+
 def interface_convert_to_mermaid(java_code, selected):
     tree = javalang.parse.parse(java_code)
     class_diagram = ""
 
+    selected_names = []
+    for c in selected:
+        selected_names.append(c["name"].split("/")[-1].split(".")[0])
+
     for path, node in tree.filter(javalang.tree.InterfaceDeclaration):
         class_name = node.name
-        extends_clause = node.extends.name if node.extends else ""
-        implements_clause = ""
+        extends_clause = []
+        if node.extends:
+            extends_clause = [imp.name for imp in node.extends]
 
-        # Generate the extends and implements clauses
+        # Generate all the imports from the same java folder
+        imports = path[0].imports
+        package = path[0].package.name
+        for imp in imports:
+            import_package = imp.path.split(".")[0]
+            import_name = imp.path.split(".")[-1]
+            if (
+                package.split(".")[0] == import_package
+                and not import_name == extends_clause
+            ):
+                # add relationship between the class and the imported class
+                # only add if the class is selected
+                for c in selected_names:
+                    if c == import_name:
+                        class_diagram += f"    {class_name} --> {import_name}\n"
+
+                    # class_diagram += f"    {class_name} --> {imp.path.split('.')[-1]}\n"
+
         if extends_clause:
-            class_diagram += f"    {class_name} <|-- {extends_clause}\n"
-        if implements_clause:
-            implements_list = [f"{class_name} <|.. {imp}" for imp in implements_clause.split(",")]
-            class_diagram += "    " + "\n    ".join(implements_list) + "\n"
+            selected_imps = [
+                imp for imp in extends_clause if selected_names.count(imp) > 0
+            ]
+            extends_list = [f"{class_name} ..|> {imp}" for imp in selected_imps]
+            class_diagram += "    " + "\n    ".join(extends_list) + "\n"
 
         # Generate the class header
         class_diagram += f"    class {class_name} {{\n"
@@ -36,7 +60,9 @@ def interface_convert_to_mermaid(java_code, selected):
         for method in node.methods:
             method_name = method.name
             return_type = method.return_type.name if method.return_type else "void"
-            parameters = ", ".join([f"{param.type.name} {param.name}" for param in method.parameters])
+            parameters = ", ".join(
+                [f"{param.type.name} {param.name}" for param in method.parameters]
+            )
             visibility = list(method.modifiers)[0] if method.modifiers else "public"
             if visibility == "private":
                 method_name = f"- {method_name}"
